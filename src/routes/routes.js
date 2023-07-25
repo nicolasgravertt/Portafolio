fd00b7b376df511186de8207aab2daa7e3431856
Nicolas Gravertt <nicolasgravertt@hotmail.com>
2023-07-25 19:05:47 -0400

    url change again

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,7 +7,7 @@ import Home from '../pages/Home/Home';
 function AppRoutes() {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      <Route exact path="/Portafolio" element={<Home />} />
       <Route exact path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   );
